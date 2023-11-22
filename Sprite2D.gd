82edae3ba7cc4d26a12bc37587595b3341c13e64
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-11-22 21:21:31 +0100

    text dialogue system

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
+const lines: Array[String] = [
+	"hey i want a coffe",
+	"A cafe latte please",
+	"Extra sugar",
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	DialogManager.start_dialog(global_position, lines)# Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
