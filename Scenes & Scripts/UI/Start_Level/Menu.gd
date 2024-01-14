73fc1eea5c24f1980ba8035d5f221ddd2f2786d9
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2024-01-14 19:35:45 +0100

    fixes. remove rendundant custom components

--- Scenes & Scripts/UI/Start_Level/Menu.gd
+++ Scenes & Scripts/UI/Start_Level/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/main_level.tscn")
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes & Scripts/Levels/main_level.tscn")
 
-func _on_options_pressed():
-	get_tree().change_scene_to_file("res://Scenes/UI/Options_menu.tscn")
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes & Scripts/UI/Options_menu.tscn")
