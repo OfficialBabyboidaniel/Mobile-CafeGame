de361043cfa87a131454ed104114cd41ed99d622
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2024-01-28 23:25:09 +0100

    fix so currency values keep between scene changes. made global singelton a scene so that global variabels can be changed in the editor. New build "update". added hud and global scene to both main scene and coffe making scene

--- Scenes & Scripts/UI/HUD/HUD.gd
+++ Scenes & Scripts/UI/HUD/HUD.gd
@@ -1,42 +1,35 @@
 class_name HUD
 extends CanvasLayer
 
-@onready var money_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Money
-@onready var gems_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Gems
-@onready var level_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Level
+var money_label 
+var gems_label 
+var level_label 
 
-var expererience = 0
-var money = 0:
-	set(new_money):
-		money = new_money
-		update_money_label()
-var gems = 0:
-	set(new_gems):
-		gems = new_gems
-		update_gems_label()
-var level = 0:
-	set(new_level):
-		level = new_level
-		update_level_label()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	money_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Money
+	gems_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Gems
+	level_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Level
 	update_money_label()
 	update_gems_label()
 	update_level_label()
 	DialogManager.order_completed.connect(add_currency_after_completed_order)
 
-
 func add_currency_after_completed_order():
-	money += 10
-	gems += 1
-	expererience += 5
-	if expererience > 10:
-		expererience = 0
-		level += 1
+	GlobalSingleton.money += 10
+	GlobalSingleton.gems += 1
+	GlobalSingleton.expererience += 5
+	if GlobalSingleton.expererience > 10:
+		GlobalSingleton.expererience = 0
+		GlobalSingleton.level += 1
+	update_money_label()
+	update_gems_label()
+	update_level_label()
 
 func update_money_label():
-	money_label.text = "Money: " + str(money)
+	money_label.text = "Money: " + str(GlobalSingleton.money)
 func update_gems_label():
-	gems_label.text = "Gems: " + str(money)
+	gems_label.text = "Gems: " + str(GlobalSingleton.gems)
 func update_level_label():
-	level_label.text = "level: " + str(money)
+	level_label.text = "level: " + str(GlobalSingleton.level)
