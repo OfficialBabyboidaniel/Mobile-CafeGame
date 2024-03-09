0fd3f9a375e4f4c576fbbb6846d86b81435e7b97
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2024-03-09 13:31:43 +0100

    -changed alot of asset names to one naming convention - continued on the drawing function - created a UI panel for the drawing part - drawing only works in the parent of which the component is added to.
    ! drawing bug, when exited out of the drawing space, re entering (while in click mode) does not draw a line. working on it.

--- Scenes & Scripts/Coffe_Making_Gameplay/Sign_Customer_Name/draw_component.gd
+++ Scenes & Scripts/Coffe_Making_Gameplay/Sign_Customer_Name/draw_component.gd
@@ -2,19 +2,27 @@ extends Node2D
 
 var drawing: bool = false
 var line: Line2D
+var was_outside: bool = true
+
 
 func _process(_delta):
-    if Input.is_action_just_pressed("Action"):
-        drawing = true
-        line = Line2D.new()
-        add_child(line)
-        line.add_point(get_global_mouse_position())
-    elif Input.is_action_just_released("Action"):
-        drawing = false
+	var mouse_pos_global = get_global_mouse_position()  # Get global mouse position
+	var parent_global_pos = get_parent().get_global_position()  # Get parent's global position
+	var mouse_pos_relative = mouse_pos_global - parent_global_pos  # Calculate mouse position relative to parent
+	var parent_rect = get_parent().get_rect()  # Get parent's local rectangle
 
-    if Input.is_action_pressed("Action") and drawing:
-        line.add_point(get_global_mouse_position())
+	if parent_rect.has_point(mouse_pos_relative):  # Check if relative mouse position is within parent's rectangle
+		if Input.is_action_just_pressed("Action"):
+			drawing = true
+			line = Line2D.new()
+			get_parent().add_child(line)
+			line.add_point(mouse_pos_relative)  # Use relative mouse position
+		elif Input.is_action_just_released("Action"):
+			drawing = false
+			line = null
 
-
-#create clear function
-
+		if Input.is_action_pressed("Action") and drawing and line != null:
+			line.add_point(mouse_pos_relative)  # Use relative mouse position
+	else:
+		drawing = false
+		line = null
