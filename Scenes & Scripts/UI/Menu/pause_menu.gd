430578fe7ab0ca49766296ece9860b571804bdf1
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2024-03-10 22:21:30 +0100

    - fix drawing component - now it draws well on the drawing space. yay

--- Scenes & Scripts/UI/Menu/pause_menu.gd
+++ Scenes & Scripts/UI/Menu/pause_menu.gd
@@ -1,8 +1,10 @@
 extends Control
 
+
 func _ready():
 	disable_all_drawing_tools()
 	%draw_component.set_process_input(true)
+
 
 func _on_pen_pressed() -> void:
 	disable_all_drawing_tools()
@@ -12,9 +14,8 @@ func _on_pen_pressed() -> void:
 func disable_all_drawing_tools() -> void:
 	for drawing_tool in get_tree().get_nodes_in_group("drawing_tools"):
 		drawing_tool.set_process_input(false)
-		
 
 
 func _on_erase_pressed() -> void:
 	disable_all_drawing_tools()
-	%erase_component.visible = true
+	%erase_component.set_process_input(true)
